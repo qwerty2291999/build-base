a43092c69d33dc8f8d5b098a1c51394ef98d7cc1
Long Dương Văn <long2291999@outlook.com>
2021-09-21 16:48:28 +0700

    nammed errors

--- errors/errors.js
+++ errors/errors.js
@@ -5,36 +5,43 @@ class ERROR extends Error {
         this.code = code
     }
     notFoundValue(value) {
+        this.name = 'Input Error'
         this.message = `Cannot find matched value ${value}`
         this.code = HTTP_CODE.NOT_FOUND
         return this
     }
     duplicateValue(value) {
+        this.name = 'Input Error'
         this.message = `Duplicate value in database ${value}`
         this.code = HTTP_CODE.NOT_ACCEPTABLE
         return this
     }
     wrongUsernameOrPassword() {
+        this.name = 'Input Error'
         this.message = 'Wrong username or password'
         this.code = HTTP_CODE.UNAUTHORIZED
         return this
     }
     notFoundURL(path) {
+        this.name = 'Path Error'
         this.message = `Requested URL not found ${path}`
         this.code = HTTP_CODE.NOT_FOUND
         return this
     }
     invalidToken() {
+        this.name = 'JWT Error'
         this.message = `Invalid Token`
         this.code = HTTP_CODE.UNAUTHORIZED
         return this
     }
     cannotDecode() {
+        this.name = 'JWT Error'
         this.message = `Failed to decode jwt`
         this.code = HTTP_CODE.UNAUTHORIZED
         return this
     }
     needToVerify() {
+        this.name = 'User Error'
         this.message = `Please verify your account first`
         this.code = HTTP_CODE.UNAUTHORIZED
         return this
